9acbed90aeb0e3414d8b6973825db02614217f85
Nacharkinaeugenia <nacharkinaeugenia@gmail.com>
2023-03-28 13:08:38 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://ce4af705-d9e7-4e4b-8899-4e3f311b6c49.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
