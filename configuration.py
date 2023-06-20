0f01201ff71af4b4c90eb7c61b4d7c4eada9b41b
Nacharkinaeugenia <nacharkinaeugenia@gmail.com>
2023-06-20 13:11:44 +0300

    portfolio

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
-URL_SERVICE = "https://ce4af705-d9e7-4e4b-8899-4e3f311b6c49.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://1ef33043-1339-4597-892e-5aaf167723cf.serverhub.praktikum-services.ru"
 DOC_PATH = "/docs/"
 LOG_MAIN_PATH = "/api/logs/main/"
 USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
-CREATE_USER_PATH = "/api/v1/users/"
+CREATE_USER_PATH =
 PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,9 +1,43 @@
 import configuration
 import requests
 import data
-def post_new_user(body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # подставялем полный url
-                         json=body,  # тут тело
-                         headers=data.headers)  # а здесь заголовки
+def get_user_body(first_name):
+    current_body = data.user_body.copy()
+    current_body["firstName"] = first_name
+    return current_body
 
-    response = post_new_user(data.user_body)
+def positive_assert(first_name):
+    user_body = get_user_body(first_name)
+    user_response = sender_stand_request.post_new_user(user_body)
+
+    assert user_response.status_code == 201
+    assert user_response.json()["authToken"] != ""
+
+    users_table_response = sender_stand_request.get_users_table()
+
+    str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
+               + user_body["address"] + ",,," + user_response.json()["authToken"]
+
+    assert users_table_response.text.count(str_user) == 1
+
+def negative_assert_symbol(first_name):
+    user_body = get_user_body(first_name)
+
+    response = sender_stand_request.post_new_user(user_body)
+
+    assert response.status_code == 400
+
+    assert response.json()["code"] == 400
+
+    assert response.json()["message"] == "Имя пользователя введено некорректно. " \
+                                         "Имя может содержать только русские или латинские буквы, " \
+                                         "Длина должна быть не менее 2 и не более 15 символов"
+
+def negative_assert_no_firstname(user_body):
+
+    response = sender_stand_request.post_new_user(user_body)
+
+    assert response.status_code == 400
+
+    assert response.json()["code"] == 400
+    assert response.json()["message"] == "Не все необходимые параметры были переданы"
